37e877bfdb1473dc4f68cbb5118ae1428ad9a6e2
Robert Klim <kontakt.robert.klim@gmail.com>
2019-01-25 17:17:02 +0100

    Add todos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const todos = document.querySelector('.collection');
+const addForm = document.getElementById('add-form');
 
 todos.addEventListener('click', (e) => {
     if (e.target.parentElement.className === 'delete-btn') {
@@ -16,4 +17,56 @@ todos.addEventListener('click', (e) => {
         // delete li element
         li.className += " done";
     }
+});
+
+addForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    
+    // create todo's elements
+    const li = document.createElement('li');
+    const div = document.createElement('div');
+    const todoText = document.createElement('span');
+    const secondaryContent = document.createElement('span');
+    const doneBtn = document.createElement('a');
+    const doneIcon = document.createElement('i');
+    const space = document.createTextNode(' ');
+    const deleteBtn = document.createElement('a');
+    const deleteIcon = document.createElement('i');
+
+    doneBtn.setAttribute('href','#!');
+    deleteBtn.setAttribute('href', '#!');
+
+    doneBtn.classList.add('done-btn');
+    deleteBtn.classList.add('delete-btn');
+
+    doneIcon.classList.add('material-icons');
+    doneIcon.textContent = 'done';
+
+    deleteIcon.classList.add('material-icons');
+    deleteIcon.textContent = 'delete';
+
+    li.classList.add('collection-item');
+
+    todoText.classList.add('todo-text');
+    todoText.textContent = value;
+
+    secondaryContent.classList.add('secondary-content');
+
+    doneBtn.appendChild(doneIcon);
+    deleteBtn.appendChild(deleteIcon);
+
+    secondaryContent.appendChild(doneBtn);
+    secondaryContent.appendChild(space);
+    secondaryContent.appendChild(deleteBtn);
+
+    div.appendChild(todoText);
+    div.appendChild(secondaryContent);
+
+    li.appendChild(div);
+
+    todos.appendChild(li);
+
+    addForm.querySelector('input[type="text"]').value = '';
+
 });
