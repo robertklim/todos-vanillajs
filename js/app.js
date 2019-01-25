8551e086ae86d6ef7e6a2bd47ccc1816857ab3f0
Robert Klim <kontakt.robert.klim@gmail.com>
2019-01-25 13:18:57 +0100

    Mark todos as done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let deleteBtns = document.querySelectorAll('.delete-btn');
+let doneBtns = document.querySelectorAll('.done-btn');
 
 deleteBtns.forEach((deleteBtn) => {
     deleteBtn.addEventListener('click', (e) => {
@@ -9,5 +10,17 @@ deleteBtns.forEach((deleteBtn) => {
         const li = e.target.closest('li');
         // delete li element
         li.parentElement.removeChild(li);
+    });
+});
+
+doneBtns.forEach((doneBtn) => {
+    doneBtn.addEventListener('click', (e) => {
+        // prevent link redirect
+        e.preventDefault();
+        // get todo's wrapping li
+        // const li = e.target.parentElement.parentElement.parentElement.parentElement;
+        const li = e.target.closest('li');
+        // mark todo as done (add done class)
+        li.className += " done";
     });
 });
