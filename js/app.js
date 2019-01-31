7512828b11b64abc248f7313592375472d8bc3e0
Robert Klim <kontakt.robert.klim@gmail.com>
2019-01-31 13:51:49 +0100

    Do some refactoring and cleanup, add comments

--- js/app.js
+++ js/app.js
@@ -1,20 +1,5 @@
+// delete or mark todos as done/to be done
 const todos = document.querySelector('.collection');
-const addForm = document.getElementById('add-form');
-const searchBar = document.forms['search-form'].querySelector('input');
-
-searchBar.addEventListener('keyup', (e) => {
-    const searchTerm = e.target.value.toLowerCase();
-    const todosAll = todos.getElementsByClassName('collection-item');
-    Array.from(todosAll).forEach((todo) => {
-        const todoText = todo.querySelector('.todo-text').textContent;
-
-        if (todoText.toLowerCase().indexOf(searchTerm) != -1) {
-            todo.classList.remove('hide');
-        } else {
-            todo.classList.add('hide');
-        }
-    });
-});
 
 todos.addEventListener('click', (e) => {
     if (e.target.parentElement.className === 'delete-btn') {
@@ -38,35 +23,20 @@ todos.addEventListener('click', (e) => {
     }
 });
 
-// todos.addEventListener('click', (e) => {
-//     if (e.target.parentElement.className === 'delete-btn') {
-//         // prevent link redirect
-//         e.preventDefault();
-//         // get todo's wrapping li
-//         const li = e.target.closest('li');
-//         // delete li element
-//         li.parentElement.removeChild(li);
-//     } else if (e.target.parentElement.className === 'done-btn') {
-//         // prevent link redirect
-//         e.preventDefault();
-//         // get todo's wrapping li
-//         const li = e.target.closest('li');
-//         // delete li element
-//         li.className += " done";
-//     }
-// });
+// add new todos
+const addForm = document.getElementById('add-form');
 
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    // get input field value
     const value = addForm.querySelector('input[type="text"]').value;
-    
+
     // create todo's elements
     const li = document.createElement('li');
     const div = document.createElement('div');
     const todoText = document.createElement('span');
     const secondaryContent = document.createElement('span');
-    // const doneBtn = document.createElement('a');
-    // const doneIcon = document.createElement('i');
     const doneCheckbox = document.createElement('label');
     const doneInput = document.createElement('input');
     const doneSpan = document.createElement('span');
@@ -74,45 +44,48 @@ addForm.addEventListener('submit', (e) => {
     const deleteBtn = document.createElement('a');
     const deleteIcon = document.createElement('i');
 
-    // doneBtn.setAttribute('href','#!');
+    // add classes and set attributes and values
     doneInput.setAttribute('type', 'checkbox');
     deleteBtn.setAttribute('href', '#!');
-
-    // doneBtn.classList.add('done-btn');
     doneInput.classList.add('filled-in');
     deleteBtn.classList.add('delete-btn');
-
-    // doneIcon.classList.add('material-icons');
-    // doneIcon.textContent = 'done';
-
     deleteIcon.classList.add('material-icons');
     deleteIcon.textContent = 'delete';
-
     li.classList.add('collection-item');
-
     todoText.classList.add('todo-text');
     todoText.textContent = value;
-
     secondaryContent.classList.add('secondary-content');
 
-    // doneBtn.appendChild(doneIcon);
+    // connect all the elements
     deleteBtn.appendChild(deleteIcon);
-
     doneCheckbox.appendChild(doneInput);
     doneCheckbox.appendChild(doneSpan);
-
     secondaryContent.appendChild(doneCheckbox);
-    // secondaryContent.appendChild(doneBtn);
     secondaryContent.appendChild(space);
     secondaryContent.appendChild(deleteBtn);
-
     div.appendChild(todoText);
     div.appendChild(secondaryContent);
-
     li.appendChild(div);
-
     todos.appendChild(li);
 
+    // clear input field
     addForm.querySelector('input[type="text"]').value = '';
 
+});
+
+// filter todos
+const searchBar = document.forms['search-form'].querySelector('input');
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    const todosAll = todos.getElementsByClassName('collection-item');
+    Array.from(todosAll).forEach((todo) => {
+        const todoText = todo.querySelector('.todo-text').textContent;
+
+        if (todoText.toLowerCase().indexOf(searchTerm) != -1) {
+            todo.classList.remove('hide');
+        } else {
+            todo.classList.add('hide');
+        }
+    });
 });
