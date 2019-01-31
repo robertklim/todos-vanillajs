19931b367db7d1bb881fb621cd67e09fabbeb7d8
Robert Klim <kontakt.robert.klim@gmail.com>
2019-01-31 11:42:21 +0100

    Use Materialize checkboxes to mark todos as done or to be done

--- js/app.js
+++ js/app.js
@@ -9,15 +9,37 @@ todos.addEventListener('click', (e) => {
         const li = e.target.closest('li');
         // delete li element
         li.parentElement.removeChild(li);
-    } else if (e.target.parentElement.className === 'done-btn') {
-        // prevent link redirect
-        e.preventDefault();
+    } else if (e.target.type === 'checkbox') {
         // get todo's wrapping li
         const li = e.target.closest('li');
-        // delete li element
-        li.className += " done";
+
+        // add or remove 'done' class
+        if (e.target.checked) {
+            li.classList.add('done');
+        } else {
+            li.classList.remove('done');
+        }
+
     }
 });
+
+// todos.addEventListener('click', (e) => {
+//     if (e.target.parentElement.className === 'delete-btn') {
+//         // prevent link redirect
+//         e.preventDefault();
+//         // get todo's wrapping li
+//         const li = e.target.closest('li');
+//         // delete li element
+//         li.parentElement.removeChild(li);
+//     } else if (e.target.parentElement.className === 'done-btn') {
+//         // prevent link redirect
+//         e.preventDefault();
+//         // get todo's wrapping li
+//         const li = e.target.closest('li');
+//         // delete li element
+//         li.className += " done";
+//     }
+// });
 
 addForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -28,20 +50,25 @@ addForm.addEventListener('submit', (e) => {
     const div = document.createElement('div');
     const todoText = document.createElement('span');
     const secondaryContent = document.createElement('span');
-    const doneBtn = document.createElement('a');
-    const doneIcon = document.createElement('i');
+    // const doneBtn = document.createElement('a');
+    // const doneIcon = document.createElement('i');
+    const doneCheckbox = document.createElement('label');
+    const doneInput = document.createElement('input');
+    const doneSpan = document.createElement('span');
     const space = document.createTextNode(' ');
     const deleteBtn = document.createElement('a');
     const deleteIcon = document.createElement('i');
 
-    doneBtn.setAttribute('href','#!');
+    // doneBtn.setAttribute('href','#!');
+    doneInput.setAttribute('type', 'checkbox');
     deleteBtn.setAttribute('href', '#!');
 
-    doneBtn.classList.add('done-btn');
+    // doneBtn.classList.add('done-btn');
+    doneInput.classList.add('filled-in');
     deleteBtn.classList.add('delete-btn');
 
-    doneIcon.classList.add('material-icons');
-    doneIcon.textContent = 'done';
+    // doneIcon.classList.add('material-icons');
+    // doneIcon.textContent = 'done';
 
     deleteIcon.classList.add('material-icons');
     deleteIcon.textContent = 'delete';
@@ -53,10 +80,14 @@ addForm.addEventListener('submit', (e) => {
 
     secondaryContent.classList.add('secondary-content');
 
-    doneBtn.appendChild(doneIcon);
+    // doneBtn.appendChild(doneIcon);
     deleteBtn.appendChild(deleteIcon);
 
-    secondaryContent.appendChild(doneBtn);
+    doneCheckbox.appendChild(doneInput);
+    doneCheckbox.appendChild(doneSpan);
+
+    secondaryContent.appendChild(doneCheckbox);
+    // secondaryContent.appendChild(doneBtn);
     secondaryContent.appendChild(space);
     secondaryContent.appendChild(deleteBtn);
 
